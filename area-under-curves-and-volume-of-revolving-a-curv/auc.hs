a32a23691da94f8532f4e961653de1abc9890077
humpass <hampus.breistein@gmail.com>
2014-02-26 21:44:15 +0100

    calculate single instance of y

---
+++ area-under-curves-and-volume-of-revolving-a-curv/auc.hs
@@ -0,0 +1,17 @@
+-- Y = (a1)xb1 + (a2)xb2 + (a3)xb3 ……(an)xbn
+as = [1, 2, 3, 4, 5]
+bs = [6, 7, 8, 9, 10]
+range = [1, 4]
+-- should give area 2435300.3
+
+
+--is the language custom 
+calcY x = foldl (\acc coeffs -> calcExpression coeffs x) 0 $ zip as bs 
+	where
+		calcExpression coeffs x = fst(coeffs)*x**snd(coeffs) -- a*x^b
+
+
+-- define the expression from the input
+-- use the expression in increments 
+
+
