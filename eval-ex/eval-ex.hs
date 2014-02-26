a240e56764320b1466587e276943bb93b94be6d2
humpass <hampus.breistein@gmail.com>
2014-02-26 14:50:47 +0100

    added missing + 1 and some clean up

--- eval-ex/eval-ex.hs
+++ eval-ex/eval-ex.hs
@@ -1,5 +1,7 @@
-solve x = sum $ map (term x) [1..9]
-                where term x n = (x**n)/(fac n)
+solve x = sum $ (:) 1 $ map (term x) [1..9]
+	where 
+		term x n = (x**n)/(fac n)
+		fac n = product [1..n]
 
-fac n = product [1..n]
 
+		  
